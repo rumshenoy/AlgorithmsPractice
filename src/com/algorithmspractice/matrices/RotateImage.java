5ab7b50a96b4c5d71f5a80228947d2ced220e8bf
INSIGHTLY\Ramya <rumshenoy@gmail.com>
2017-12-27 16:21:50 -0800

    Leetcode - 98. Validate Binary Search Tree

---
+++ src/com/algorithmspractice/matrices/RotateImage.java
@@ -0,0 +1,75 @@
+package com.algorithmspractice.matrices;
+//You are given an n x n 2D matrix representing an image.
+//
+//Rotate the image by 90 degrees (clockwise).
+//
+//Note:
+//You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+//
+//Example 1:
+//
+//Given input matrix =
+//[
+//[1,2,3],
+//[4,5,6],
+//[7,8,9]
+//],
+//
+//rotate the input matrix in-place such that it becomes:
+//[
+//[7,4,1],
+//[8,5,2],
+//[9,6,3]
+//]
+//Example 2:
+//
+//Given input matrix =
+//[
+//[ 5, 1, 9,11],
+//[ 2, 4, 8,10],
+//[13, 3, 6, 7],
+//[15,14,12,16]
+//],
+//
+//rotate the input matrix in-place such that it becomes:
+//[
+//[15,13, 2, 5],
+//[14, 3, 4, 1],
+//[12, 6, 8, 9],
+//[16, 7,10,11]
+//]
+public class RotateImage {
+    public static void main(String[] args) {
+        RotateImage rotateImage = new RotateImage();
+        int[][] matrix =
+                {{1, 4, 7},
+                {2, 8, 3},
+                {9, 5, 6}};
+        rotateImage.rotate(matrix);
+
+        for(int i =0; i < matrix.length; i++){
+            for(int j =0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        for(int i =0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+    }
+}
